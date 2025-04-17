619bf67317a3f5b3457684db22a9188322837de3
dcastillo-highlights <darda@thehighlightsapp.com>
2025-04-17 15:17:20 -0400

    Added checkIfTakeable

--- src/take.ts
+++ src/take.ts
@@ -15,7 +15,15 @@ interface HandleTakeParams {
   signer: Signer;
   pool: FungiblePool;
   poolConfig: RequireFields<PoolConfig, 'take'>;
-  config: Pick<KeeperConfig, 'dryRun' | 'subgraphUrl' | 'delayBetweenActions' | 'connectorTokens' | 'oneInchRouters' | 'keeperTaker'>;
+  config: Pick<
+    KeeperConfig,
+    | 'dryRun'
+    | 'subgraphUrl'
+    | 'delayBetweenActions'
+    | 'connectorTokens'
+    | 'oneInchRouters'
+    | 'keeperTaker'
+  >;
 }
 
 export async function handleTakes({
@@ -65,7 +73,10 @@ interface LiquidationToTake {
 
 interface GetLiquidationsToTakeParams
   extends Pick<HandleTakeParams, 'pool' | 'poolConfig' | 'signer'> {
-  config: Pick<KeeperConfig, 'subgraphUrl'>;
+  config: Pick<
+    KeeperConfig,
+    'subgraphUrl' | 'oneInchRouters' | 'connectorTokens'
+  >;
 }
 
 async function checkIfArbTakeable(
@@ -81,8 +92,13 @@ async function checkIfArbTakeable(
     return { isTakeable: false, hpbIndex: 0 };
   }
 
-  const collateralDecimals = await getDecimalsErc20(signer, pool.collateralAddress);
-  const minCollateral = ethers.BigNumber.from(decimaledToWei(config.take.minCollateral, collateralDecimals));
+  const collateralDecimals = await getDecimalsErc20(
+    signer,
+    pool.collateralAddress
+  );
+  const minCollateral = ethers.BigNumber.from(
+    decimaledToWei(config.take.minCollateral, collateralDecimals)
+  );
   if (collateral.lt(minCollateral)) {
     logger.debug(
       `Collateral ${collateral} below minCollateral ${minCollateral} for pool: ${pool.name}`
@@ -100,12 +116,99 @@ async function checkIfArbTakeable(
   }
 
   const hmbIndex = buckets[0].bucketIndex;
-  const hmbPrice = Number(weiToDecimaled(pool.getBucketByIndex(hmbIndex).price));
+  const hmbPrice = Number(
+    weiToDecimaled(pool.getBucketByIndex(hmbIndex).price)
+  );
   const maxArbPrice = hmbPrice * config.take.hpbPriceFactor;
   return {
     isTakeable: price < maxArbPrice,
     hpbIndex: hmbIndex,
   };
+}
+
+async function checkIfTakeable(
+  pool: FungiblePool,
+  price: number,
+  collateral: BigNumber,
+  config: RequireFields<PoolConfig, 'take'>,
+  signer: Signer,
+  oneInchRouters: { [chainId: number]: string } | undefined,
+  connectorTokens: string[] | undefined
+): Promise<{ isTakeable: boolean }> {
+  if (
+    config.take.liquiditySource !== LiquiditySource.ONEINCH ||
+    !config.take.marketPriceFactor
+  ) {
+    return { isTakeable: false };
+  }
+
+  if (!collateral.gt(0)) {
+    logger.debug(
+      `Invalid collateral amount: ${collateral.toString()} for pool ${pool.name}`
+    );
+    return { isTakeable: false };
+  }
+
+  try {
+    const chainId = await signer.getChainId();
+    if (!oneInchRouters || !oneInchRouters[chainId]) {
+      logger.debug(
+        `No 1inch router configured for chainId ${chainId} in pool ${pool.name}`
+      );
+      return { isTakeable: false };
+    }
+
+    const dexRouter = new DexRouter(signer, {
+      oneInchRouters: oneInchRouters ?? {},
+      connectorTokens: connectorTokens ?? [],
+    });
+    const quoteResult = await dexRouter.getQuoteFromOneInch(
+      chainId,
+      collateral,
+      pool.collateralAddress,
+      pool.quoteAddress
+    );
+
+    if (!quoteResult.success) {
+      logger.debug(
+        `No valid quote data for collateral ${ethers.utils.formatUnits(collateral, await getDecimalsErc20(signer, pool.collateralAddress))} in pool ${pool.name}: ${quoteResult.error}`
+      );
+      return { isTakeable: false };
+    }
+
+    const amountOut = ethers.BigNumber.from(quoteResult.dstAmount);
+    if (amountOut.isZero()) {
+      logger.debug(
+        `Zero amountOut for collateral ${ethers.utils.formatUnits(collateral, await getDecimalsErc20(signer, pool.collateralAddress))} in pool ${pool.name}`
+      );
+      return { isTakeable: false };
+    }
+
+    const collateralDecimals = await getDecimalsErc20(
+      signer,
+      pool.collateralAddress
+    );
+    const quoteDecimals = await getDecimalsErc20(signer, pool.quoteAddress);
+
+    const collateralAmount = Number(
+      ethers.utils.formatUnits(collateral, collateralDecimals)
+    );
+    const quoteAmount = Number(
+      ethers.utils.formatUnits(amountOut, quoteDecimals)
+    );
+
+    const marketPrice = quoteAmount / collateralAmount;
+    const takeablePrice = marketPrice * config.take.marketPriceFactor;
+
+    logger.debug(
+      `Market price: ${marketPrice}, takeablePrice: ${takeablePrice}, liquidation price: ${price} for pool ${pool.name}`
+    );
+
+    return { isTakeable: price <= takeablePrice };
+  } catch (error) {
+    logger.error(`Failed to fetch quote data for pool ${pool.name}: ${error}`);
+    return { isTakeable: false };
+  }
 }
 
 export async function* getLiquidationsToTake({
@@ -114,7 +217,7 @@ export async function* getLiquidationsToTake({
   signer,
   config,
 }: GetLiquidationsToTakeParams): AsyncGenerator<LiquidationToTake> {
-  const { subgraphUrl } = config;
+  const { subgraphUrl, oneInchRouters, connectorTokens } = config;
   const minCollateral = poolConfig.take.minCollateral ?? 0;
   const {
     pool: { hpb, hpbIndex, liquidationAuctions },
@@ -126,10 +229,19 @@ export async function* getLiquidationsToTake({
 
   const DEFAULT_MIN_DEPOSIT = '0.001';
   let minDeposit = DEFAULT_MIN_DEPOSIT;
-  if (poolConfig.take.minCollateral && hpb && !ethers.BigNumber.from(hpb).isZero()) {
-    const minCollateralBN = ethers.BigNumber.from(decimaledToWei(poolConfig.take.minCollateral, 18));
+  if (
+    poolConfig.take.minCollateral &&
+    hpb &&
+    !ethers.BigNumber.from(hpb).isZero()
+  ) {
+    const minCollateralBN = ethers.BigNumber.from(
+      decimaledToWei(poolConfig.take.minCollateral, 18)
+    );
     const hpbBN = ethers.BigNumber.from(hpb);
-    minDeposit = minCollateralBN.mul(ethers.utils.parseUnits('1', 18)).div(hpbBN).toString();
+    minDeposit = minCollateralBN
+      .mul(ethers.utils.parseUnits('1', 18))
+      .div(hpbBN)
+      .toString();
   }
 
   for (const auction of liquidationAuctions) {
@@ -140,12 +252,25 @@ export async function* getLiquidationsToTake({
 
     // TODO: Create a `checkIfTakeable` function which calculates takeablePrice based on configuration and
     // liquiditySource (1inch) API
-    const takeablePrice = 0;
-    if (price <= takeablePrice && poolConfig.take.liquiditySource === LiquiditySource.ONEINCH && poolConfig.take.marketPriceFactor) {
+    const { isTakeable: isTakeableForTake } = await checkIfTakeable(
+      pool,
+      price,
+      collateral,
+      poolConfig,
+      signer,
+      oneInchRouters,
+      connectorTokens
+    );
+    if (isTakeableForTake) {
       logger.debug(
-        `Found liquidation to take - pool: ${pool.name}, borrower: ${borrower}, price: ${price}, takeablePrice: ${takeablePrice}`
+        `Found liquidation to take - pool: ${pool.name}, borrower: ${borrower}, price: ${price}`
       );
-      yield { takeStrategy: TakeStrategy.Take, borrower, hpbIndex: 0, collateral };
+      yield {
+        takeStrategy: TakeStrategy.Take,
+        borrower,
+        hpbIndex: 0,
+        collateral,
+      };
       continue;
     }
 
@@ -163,7 +288,12 @@ export async function* getLiquidationsToTake({
         logger.debug(
           `Found liquidation to arbTake - pool: ${pool.name}, borrower: ${borrower}, price: ${price}`
         );
-        yield { takeStrategy: TakeStrategy.ArbTake, borrower, hpbIndex: arbHpbIndex, collateral };
+        yield {
+          takeStrategy: TakeStrategy.ArbTake,
+          borrower,
+          hpbIndex: arbHpbIndex,
+          collateral,
+        };
       } else {
         logger.debug(
           `Not taking liquidation since price is too high for pool: ${pool.name}, borrower: ${borrower}, price: ${price}`
@@ -176,7 +306,10 @@ export async function* getLiquidationsToTake({
 interface TakeLiquidationParams
   extends Pick<HandleTakeParams, 'pool' | 'poolConfig' | 'signer'> {
   liquidation: LiquidationToTake;
-  config: Pick<KeeperConfig, 'dryRun' | 'connectorTokens' | 'oneInchRouters' | 'keeperTaker'>;
+  config: Pick<
+    KeeperConfig,
+    'dryRun' | 'connectorTokens' | 'oneInchRouters' | 'keeperTaker'
+  >;
 }
 
 export async function takeLiquidation({
@@ -193,28 +326,42 @@ export async function takeLiquidation({
     logger.info(
       `DryRun - would Take - poolAddress: ${pool.poolAddress}, borrower: ${borrower} using ${poolConfig.take.liquiditySource}`
     );
+    return;
   }
 
-  if (poolConfig.take.liquiditySource !== LiquiditySource.ONEINCH || !poolConfig.take.marketPriceFactor) {
+  if (
+    poolConfig.take.liquiditySource !== LiquiditySource.ONEINCH ||
+    !poolConfig.take.marketPriceFactor
+  ) {
     logger.error(
       `Valid liquidity source not configured or marketPriceFactor missing. Skipping liquidation of poolAddress: ${pool.poolAddress}, borrower: ${borrower}`
     );
     return;
   }
-    
+
   const dexRouter = new DexRouter(signer, {
     oneInchRouters: config.oneInchRouters ?? {},
     connectorTokens: config.connectorTokens ?? [],
   });
-  const swapData = await dexRouter.getSwapDataFromOneInch(
-        await signer.getChainId(),
-        liquidation.collateral,
-        pool.collateralAddress,
-        pool.quoteAddress,
-        1,
-        await signer.getAddress(),
-        true,
-      );
+  const swapDataResult = await dexRouter.getSwapDataFromOneInch(
+    await signer.getChainId(),
+    collateral,
+    pool.collateralAddress,
+    pool.quoteAddress,
+    1,
+    await signer.getAddress(),
+    true
+  );
+
+  if (!swapDataResult.success || !swapDataResult.data) {
+    logger.error(
+      `Failed to get swap data for pool ${pool.name}: ${swapDataResult.error}`
+    );
+    return;
+  }
+
+  const swapData = swapDataResult.data;
+  // TODO: need to encode and pass OneInchSwapDetails
   // const swapCalldata: SwapCalldata = decodeSwapCalldata(swapData.data);
   // const swapDetails = {
   //   aggregationExecutor: swapCalldata.aggregationExecutor,
@@ -222,16 +369,19 @@ export async function takeLiquidation({
   //   opaqueData: swapCalldata.encodedCalls,
   // }
 
-  const keeperTaker = AjnaKeeperTaker__factory.connect(config.keeperTaker!!, signer);
+  const keeperTaker = AjnaKeeperTaker__factory.connect(
+    config.keeperTaker!!,
+    signer
+  );
   // TODO: need to encode and pass OneInchSwapDetails as last parameter
-  /*const tx = await keeperTaker.takeWithAtomicSwap(
-    pool.poolAddress,
-    liquidation.borrower,
-    liquidation.collateral,
-    poolConfig.take.liquiditySource,
-    dexRouter.getRouter(await signer.getChainId())!!,
-    swapDetails, // TODO: Need to abi.encode
-  );*/
+  // const tx = await keeperTaker.takeWithAtomicSwap(
+  //   pool.poolAddress,
+  //   borrower,
+  //   collateral,
+  //   poolConfig.take.liquiditySource,
+  //   dexRouter.getRouter(await signer.getChainId())!,
+  //   swapDetails
+  // );
 }
 
 interface ArbTakeLiquidationParams
